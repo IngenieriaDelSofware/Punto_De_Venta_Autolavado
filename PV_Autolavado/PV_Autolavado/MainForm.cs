902f87da86297c29a8ee4e49febd51104399504f
emfrigo <emfrigo@hotmail.com>
2014-03-12 22:53:04 -0600

    + Se sube proyecto base

---
+++ PV_Autolavado/PV_Autolavado/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sdarkland
+ * Date: 12/03/2014
+ * Time: 10:51 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PV_Autolavado
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
